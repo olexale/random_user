6b5ac285faf1fa038a35681d5efb582fea2ae607
Oleksandr Leuschenko <olexa.le@gmail.com>
2019-01-21 20:04:10 +0200

    Add basic CI

---
+++ tool/publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+mkdir -p .pub-cache
+
+cat <<EOF > ~/.pub-cache/credentials.json
+{
+  "accessToken":"$accessToken",
+  "refreshToken":"$refreshToken",
+  "tokenEndpoint":"$tokenEndpoint",
+  "scopes":["$scopes"],
+  "expiration":$expiration
+}
+EOF
+
+pub get
+pub run test_coverage
+coveralls-lcov --repo-token $COVERALLS_TOKEN coverage/lcov.info
+pub publish -f
